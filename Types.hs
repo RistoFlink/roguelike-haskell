bfb730abdcd7c6cee161143e9fcc70157110730d
RistoFlink <kenssi@gmail.com>
2025-11-30 09:43:27 +0200

    Finish the types

--- Types.hs
+++ Types.hs
@@ -25,3 +25,39 @@ data Tile
   | Wall
   | Door
   deriving (Eq, Show)
+
+-- Monster with position, type, and health
+data Monster = Monster
+  { mPos :: Position,
+    mType :: Entity,
+    mHealth :: Int
+  }
+  deriving (Eq, Show)
+
+-- Item with position and type
+data Item = Item
+  { iPos :: Position,
+    iType :: Entity
+  }
+  deriving (Eq, Show)
+
+-- Complete game state
+data GameState = GameState
+{ playerPos :: Position
+, playerHealth :: Int
+, playerMaxHealth :: Int
+, playerAttack :: Int
+, dungeon :: [Tile]
+, monsters :: [Monster]
+, items :: [Item]
+, message :: String
+, gameOver :: Bool
+, rng :: StdGen
+} deriving (Show)
+
+-- Constants
+dungeonWidth :: Int
+dungeonWidth = 50
+
+dungeonHeight :: Int
+dungeonHeight = 20
