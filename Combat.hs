a2cef4033844a307059bef710f06f1479c7c2376
RistoFlink <kenssi@gmail.com>
2025-12-07 12:32:07 +0200

    Implement basic form of combat

---
+++ Combat.hs
@@ -0,0 +1,47 @@
+module Combat
+  ( attackMonster,
+    movePlayer,
+  )
+where
+
+import Data.List (delete, find)
+import Data.Maybe (fromJust, isJust)
+import Dungeon (getTile)
+import Entity (moveMonsters, pickupItem)
+import Types
+
+-- Move the player to a new position and handle interactions
+movePlayer :: Position -> GameState -> GameState
+movePlayer newPos state
+  | getTile (dungeon state) newPos /= Floor =
+      state {message = "You bump into a wall."}
+  | isJust monsterHere =
+      attackMonster (fromJust monsterHere) state
+  | isJust itemHere =
+      pickupItem (fromJust itemHere) state {playerPos = newPos}
+  | otherwise =
+      moveMonsters $ state {playerPos = newPos, message = ""}
+  where
+    monsterHere = find (\m -> mPos m == newPos) (monsters state)
+    itemHere = find (\i -> iPos i == newPos) (items state)
+
+-- Attack a monster and update the game state
+attackMonster :: Monster -> GameState -> GameState
+attackMonster monster state =
+  let damage = playerAttack state
+      newHealth = mHealth monster - damage
+      newMonsters =
+        if newHealth <= 0
+          then delete monster (monsters state)
+          else map updateMonster (monsters state)
+      msg =
+        if newHealth <= 0
+          then "You defeated the " ++ show (mType monster) ++ "!"
+          else "You hit the " ++ show (mType monster) ++ " for " ++ show damage ++ " damage!"
+      stateAfterAttack = state {monsters = newMonsters, message = msg}
+   in moveMonsters stateAfterAttack
+  where
+    updateMonster m =
+      if m == monster
+        then m {mHealth = newHealth}
+        else m
