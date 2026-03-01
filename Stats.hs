87b452bf2373fead3ba6af27c2247d10c5796dda
RistoFlink <kenssi@gmail.com>
2026-03-01 08:47:18 +0200

    Add function for stats boosts

--- Stats.hs
+++ Stats.hs
@@ -19,6 +19,14 @@ getModifier n = (n - 10) `div` 2
 baseStats :: Stats
 baseStats = Stats 10 10 10 10 10 10
 
+applyBoost :: Ability -> Stats -> Stats
+applyBoost Str s = s {str = str s + 2}
+applyBoost Dex s = s {dex = dex s + 2}
+applyBoost Con s = s {con = con s + 2}
+applyBoost Int s = s {int = int s + 2}
+applyBoost Wis s = s {wis = wis s + 2}
+applyBoost Cha s = s {cha = cha s + 2}
+
 -- Armor class and attack bonuses
 getArmorClass :: Stats -> Int
 getArmorClass s = 10 + getModifier (dex s)
