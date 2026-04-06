5e266cbb7c3250b7bd52733c44b0b70d07f2e09a
RistoFlink <kenssi@gmail.com>
2026-04-06 13:25:48 +0300

    Skeleton for flavour texts

---
+++ Flavor.hs
@@ -0,0 +1,12 @@
+module Flavor (getAncestryFlavor, getBackgroundFlavor, getClassFavor) where
+
+import Types
+
+getAncestryFlavor :: Ancestry -> String
+getAncestryFlavor _ = "Coming soon.."
+
+getBackgroundFlavor :: Background -> String
+getBackgroundFlavor _ = "Coming soon"
+
+getClassFlavor :: Class -> String
+getClassFlavor _ = "Coming soon.."
